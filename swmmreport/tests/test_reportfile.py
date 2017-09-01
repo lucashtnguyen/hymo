c1cfa886ccbe76436c31fa348ac4f6abeef6f561
Lucas Nguyen <LNguyen@Geosyntec.com>
2017-09-01 15:24:59 -0700

    Added more complete test skeleton

--- swmmreport/tests/test_reportfile.py
+++ swmmreport/tests/test_reportfile.py
@@ -19,6 +19,12 @@ class base_ReportFileMixin(object):
     known_node_depth_columns = ['Type', 'Avg_Depth_Feet', 'Max_Depth_Feet',
         'Max_HGL_Feet', 'Day_of_max', 'Time_of_max', 'Reported_Max'
     ]
+    known_node_infow_columns = ['Node', 'Type',
+        'Maximum_Lateral_Inflow_cfs', 'Maximum_Total_Inflow_CFS',
+        'Time_of_Max_Occurrence_days', 'Time_of_Max_Occurrence_hours',
+        'Lateral_Inflow_Volume_mgals', 'Total_Inflow_Volume_mgals',
+        'Flow_Balance_Error_Percent', 'flag'
+    ]
     def teardown(self):
         None
 
@@ -41,24 +47,45 @@ class base_ReportFileMixin(object):
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'node_inflow_results')
+            assert isinstance(self.rpt.node_inflow_results, pd.DataFrame)
+            for col in self.known_node_inflow_columns:
+                assert col in self.rpt.node_inflow_results.columns.tolist()
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'node_flooding_results')
+            assert isinstance(self.rpt.node_flooding_results, pd.DataFrame)
+            for col in self.known_node_flooding_columns:
+                assert col in self.rpt.node_flooding_results.columns.tolist()
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'storage_volume_results')
+            assert isinstance(self.rpt.storage_volume_results, pd.DataFrame)
+            for col in self.known_storage_volume_columns:
+                assert col in self.rpt.storage_volume_results.columns.tolist()
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'outfall_loading_results')
+            assert isinstance(self.rpt.outfall_loading_results, pd.DataFrame)
+            for col in self.known_outfall_loading_columns:
+                assert col in self.rpt.outfall_loading_results.columns.tolist()
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'link_flow_results')
+            assert isinstance(self.rpt.link_flow_results, pd.DataFrame)
+            for col in self.known_link_flow_columns:
+                assert col in self.rpt.link_flow_results.columns.tolist()
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'flow_classification_results')
+            assert isinstance(self.rpt.flow_classification_results, pd.DataFrame)
+            for col in self.known_flow_classification_columns:
+                assert col in self.rpt.flow_classification_results.columns.tolist()
 
         with pytest.raises(NotImplementedError):
             assert hasattr(self.rpt, 'conduit_surcharge_results')
+            assert isinstance(self.rpt.conduit_surcharge_results, pd.DataFrame)
+            for col in self.known_conduit_surcharge_columns:
+                assert col in self.rpt.conduit_surcharge_results.columns.tolist()
 
 
 class Test_ReportFile(base_ReportFileMixin):
