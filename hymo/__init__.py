c7977f26b0b5b8056cfe0a6a6e79278a4aa2967e
Austin Orr <AOrr@geosyntec.com>
2018-04-24 11:16:13 -0700

    added class for parsing a swmm5 interface file

--- hymo/__init__.py
+++ hymo/__init__.py
@@ -1,5 +1,6 @@
 from .swmmreport import SWMMReportFile
 from .swmminp import SWMMInpFile
+from .swmminterface import SWMMInterfaceFile
 from .lspcreport import LSPCResultsFile
 from .lspcinp import LSPCInpFile
 from .tests import test
